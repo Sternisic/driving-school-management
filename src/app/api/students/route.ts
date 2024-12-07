9b2ceaed40980c1f5ec01b3fa04921be4605e7eb
Sternisic <sternisic.code@gmail.com>
2024-12-07 23:01:23 +0100

    students route

--- src/app/api/students/route.ts
+++ src/app/api/students/route.ts
@@ -1,23 +1,27 @@
-import { NextResponse } from 'next/server';
-import { PrismaClient } from '@prisma/client';
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-// GET: Einzelnen Schüler abrufen
+// GET: Alle Schüler abrufen
 export async function GET() {
   try {
     const students = await prisma.student.findMany(); // Alle Schüler abrufen
     return NextResponse.json(students);
   } catch (error) {
     console.error("Fehler beim Abrufen der Schüler:", error);
-    return NextResponse.json({ error: "Fehler beim Abrufen der Schüler" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Fehler beim Abrufen der Schüler" },
+      { status: 500 }
+    );
   }
 }
 
 // POST: Schüler hinzufügen
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
   try {
     const data = await req.json(); // Schülerdaten aus dem Request-Body
+
     const newStudent = await prisma.student.create({
       data: {
         firstName: data.firstName,
@@ -27,17 +31,21 @@ export async function POST(req: Request) {
         gearType: data.gearType,
         address: data.address,
         postalCode: data.postalCode, // Neue Felder hinzufügen
-        birthDate: new Date(data.birthDate), // Datum in Date-Objekt umwandeln
+        birthDate: data.birthDate ? new Date(data.birthDate) : null, // Datum in Date-Objekt umwandeln
         birthPlace: data.birthPlace,
         nationality: data.nationality,
         occupation: data.occupation,
-        lessons: data.lessons,
-        specialTrips: data.specialTrips,
+        lessons: data.lessons || 0, // Standardwert 0, falls nicht angegeben
+        specialTrips: data.specialTrips || {}, // Standardwert leeres Objekt, falls nicht angegeben
       },
     });
+
     return NextResponse.json(newStudent);
   } catch (error) {
-    console.error('Fehler beim Hinzufügen eines Schülers:', error);
-    return NextResponse.json({ error: 'Fehler beim Hinzufügen eines Schülers' }, { status: 500 });
+    console.error("Fehler beim Hinzufügen eines Schülers:", error);
+    return NextResponse.json(
+      { error: "Fehler beim Hinzufügen eines Schülers" },
+      { status: 500 }
+    );
   }
 }
